9bb11782ea1ef1ad4545da27ae22496fac82daae
Cassandra <McClure>
2018-08-27 16:40:19 -0700

    Create Board#board instance variable nilcase

--- chess/board.rb
+++ chess/board.rb
@@ -1,17 +1,30 @@
 class Board 
   attr_reader :board 
   def initialize(board = nil)
+    # fill_row = Array.new(8) {Piece.new}
+    # empty_row = Array.new(8) {[]}
     if board.nil?
-      @board = Array.new(8) {Array.new(8){[]}}
+      @board =  Array.new(8) {Array.new(8){[]}}
+      populate
     else 
       @board = board
     end 
-    
   end 
   
+  def populate
+    @board[0] = Array.new(8) {Piece.new}
+    @board[1] = Array.new(8) {Piece.new}
+    @board[6] = Array.new(8) {Piece.new}
+    @board[7] = Array.new(8) {Piece.new}
+  end 
+end 
+
+class Piece 
   
+  def initialize 
+  end 
   
 end 
-x = Board.new
 
+x = Board.new
 p x.board
