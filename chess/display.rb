487bcb1d4de32d867185650c7922f96bc7564ec1
Cassandra <McClure>
2018-08-28 14:59:09 -0700

    Create Display#render_loop method with bugs - issue with update @cursor.cursor_pos

--- chess/display.rb
+++ chess/display.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require 'colorize'
 require_relative 'board.rb'
 require_relative 'cursor.rb'
@@ -11,13 +12,27 @@ class Display
   def render
     curs_pos = @cursor.cursor_pos
     @board.board.each do |el|
-      el.each do |piece|
-        puts piece unless piece == @board[curs_pos]
-        puts @board[curs_pos].render.colorize(:blue)
+      el.each_with_index do |piece, idx|
+        if piece == @board[curs_pos]
+          print @board[curs_pos].render.colorize(:red)
+        elsif idx == 7
+          puts piece.render
+        else
+          print piece.render
+        end
       end
     end
     # puts @board[pos].render.colorize(:blue)
+    curs_pos = nil
     @cursor.get_input
+  end
+
+  def render_loop
+    p "ctr-c to exit"
+    while true
+      render
+      puts "---"
+    end
   end
 
 end
