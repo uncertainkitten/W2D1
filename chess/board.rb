4b07d2a22edc66f1cf6e9424d64847ab07dc03f6
Cassandra <McClure>
2018-08-27 17:29:37 -0700

    Fix Board#move_piece conditional

--- chess/board.rb
+++ chess/board.rb
@@ -1,4 +1,5 @@
 class Board 
+  require 'byebug'
   attr_reader :board 
   def initialize(board = nil)
     # fill_row = Array.new(8) {Piece.new}
@@ -12,11 +13,6 @@ class Board
   end 
   
   def populate
-    # @board[0] = Array.new(8) {Piece.new}
-    # @board[1] = Array.new(8) {Piece.new}
-    # @board[6] = Array.new(8) {Piece.new}
-    # @board[7] = 
-    
     @board.each_with_index do |el,idx|
       if (idx < 2) || (idx > 5)
         board[idx] = Array.new(8) {Piece.new}
@@ -25,11 +21,42 @@ class Board
       end
     end
   end 
+  
+  def pos_parse(pos)
+    @board[pos[0]][pos[1]]
+  end 
+  
+  def []=(pos,piece)
+    @board[pos[0]][pos[1]] = piece
+  end
+  
+  def move_piece(start_pos, end_pos)
+    if is_valid?(start_pos, end_pos)
+      held_piece = pos_parse(start_pos)
+      move_to = pos_parse(end_pos)
+      @board.[]=(start_pos, move_to)
+      @board.[]=(end_pos, held_piece)
+    end   
+  end
+  
+  def is_valid?(start_pos, end_pos)
+    begin
+      raise MissingPiece, "No piece!" if start_pos.class == NullPiece
+      raise InvalidMove, "Out of bounds on row" if (end_pos[0] > 7) || (end_pos[0] < 0)
+      raise InvalidMove, "Out of bounds on column" if (end_pos[1] > 7) || (end_pos[1] < 0)
+      raise InvalidMove, "Already piece in square" unless pos_parse(end_pos).class == NullPiece
+      # raise InvalidMove, "Piece doesn't move that way!" if piece.valid_move(start_pos, end_pos)
+    end
+    
+    return true
+  end 
+  
 end 
 
 class Piece 
   
-  def initialize 
+  def initialize
+    @pos = [0,0]
   end 
   
 end 
@@ -39,5 +66,14 @@ class NullPiece < Piece
   end 
 end
 
+class InvalidMove < ArgumentError
+end 
+
+class MissingPiece < ArgumentError
+end
+
 x = Board.new
+p x.board[2][1]
+p x.board[1][1]
+x.move_piece([1,1],[2,1])
 p x.board
